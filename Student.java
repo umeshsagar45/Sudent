c3fa59b4eda9fb95d5b8c902eda90085319b268f
umeshsagar45 <umeshsagar411@gmail.com>
2025-11-17 12:54:47 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -7,8 +7,9 @@ public class Student {
         System.out.println("My Name is: " + sname);
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
-        System.out.println("=======================");
          System.out.println("WELCOME TO JENKINS");
+        System.out.println("code chnages, job runs");
+        
         
     }
 }
